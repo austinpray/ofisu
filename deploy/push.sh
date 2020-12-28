55948d53023f9739c2f5aa8a46d0022b9996697d
Austin Pray <austin@austinpray.com>
2020-12-28 14:06:10 -0600

    deploy

--- deploy/push.sh
+++ deploy/push.sh
@@ -6,6 +6,7 @@ BASEDIR=$(dirname "$0")
 OFISU_CONTAINER_BASE="us.gcr.io/kizuna-188702/github.com/austinpray/ofisu"
 OFISU_CONTAINER_TAG="$(git branch --show-current)-$(git rev-parse HEAD)"
 OFISU_CONTAINER_NAME="$OFISU_CONTAINER_BASE:$OFISU_CONTAINER_TAG"
+export OFISU_CONTAINER_NAME
 echo "BUILDING $OFISU_CONTAINER_NAME"
 docker-compose build
 
